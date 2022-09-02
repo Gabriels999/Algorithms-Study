c42056b473d879a8c0e8a9ff1c6d82841d40522a
Gabriel Santos <gab.tec97@gmail.com>
2022-09-02 19:37:44 -0300

    Tentando resolver a aposta dos gnomos

--- cifra_de_cesar.py
+++ cifra_de_cesar.py
@@ -1,11 +1,18 @@
-entrada = input()
+# https://www.beecrowd.com.br/judge/pt/problems/view/1253
+
+qtd_casos = input()
+codigo_asc_transformado = ''
 resposta = ''
 
-for numero in range(int(entrada)):
+for numero in range(int(qtd_casos)):
     palavra = input()
-    chave = input()
+    chave = int(input())
     for letra in palavra:
-        codigo_numerico = ord(letra)
-        resposta += chr(int(codigo_numerico)-int(chave))
+        codigo_asc = ord(letra)
+        codigo_asc_transformado = codigo_asc - chave
+        if codigo_asc_transformado < 65:
+            codigo_asc_transformado+=26
+        resposta += chr(codigo_asc_transformado)
     print(resposta)
     resposta = ''
+
