a2d393032e0cb5f8f6a9a300e506f7c57bc13535
Gabriel Santos <gab.tec97@gmail.com>
2023-12-03 15:37:04 -0300

    first commit pt.1

--- AdventureOfCode/2023/day_1_joining_digits.py
+++ AdventureOfCode/2023/day_1_joining_digits.py
@@ -1,31 +1,16 @@
 from utils import read_file
 
 
-def check_digits_letters_presence(text: str):
-    base = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
-    return [number for number in base if number in text]
-
 def get_digits(text: str) -> int:
-    # digits_writtens = check_digits_letters_presence(text)
-
-    # if not digits_writtens:
-        first_digit = [digit for digit in text if digit.isdigit()][0]
-        last_digit = [digit for digit in reversed(text) if digit.isdigit()][0]
-        return first_digit + last_digit
-
-    
-
-    # for letter in text:
-
-
+    first_digit = [digit for digit in text if digit.isdigit()][0]
+    last_digit = [digit for digit in reversed(text) if digit.isdigit()][0]
+    return first_digit + last_digit
 
 
 def solve_puzzle():
-    # data = read_file(f'AdventureOfCode/2023/inputs/day1.txt')
-    data = read_file(f'AdventureOfCode/2023/inputs/day1-test.txt')
+    data = read_file(f'AdventureOfCode/2023/inputs/day1.txt')
     total = 0
     for line in data:
-        line_copy = line
         total+= int(get_digits(line))
     return total
 
